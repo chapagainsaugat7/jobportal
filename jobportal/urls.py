29ac3cf84e4f7d8b5654a92bb2ab0a921ba84d7e
Rajuxz <rajankhanalxz@gmail.com>
2023-07-18 09:05:04 +0545

    Redirecting urls to the apps: employers and jobseeker

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
+    path('job-seeker/',include('jobseeker.urls')),
+    path('employer/',include('employer.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
