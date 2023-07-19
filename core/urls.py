9ae2839a6f08e89dadacea93504a569f2480635f
Rajuxz <rajankhanalxz@gmail.com>
2023-07-19 22:42:11 +0545

    Fix: statistics template not found error

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home,name="home_page"),    
+    path('',home,name="home_page"),
     path('portal-admin',admin,name='admin'),
     path('portal-test',admin_test,name='admin-test'),
-    path('admin_home/',admin_home,name="admin_home")
+    path('admin_home/',admin_home,name="home_page"),
+    path('statistics',statistics,name="statistics"),
 ]
