f12939f575629a3935e130be437163d217a1773a
Rajuxz <rajankhanalxz@gmail.com>
2023-09-26 11:49:56 +0545

    Handeling data from view

--- employer/views.py
+++ employer/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
-from django.http import JsonResponse
+from django.contrib import messages
+from django.http import JsonResponse,HttpResponse
 from .models import Employer
+from django.contrib.auth.hashers import make_password,check_password
+
 # Create your views here.
 
 
@@ -14,22 +17,24 @@ def register_employer(request):
         number = request.POST.get('number')
         email = request.POST.get('email')
         password = request.POST.get('password')
-
+        hashed_password = make_password(password)
         # normaline email, 
         normalized_email = email.lower()
 
         if Employer.objects.filter(emp_email = normalized_email):
-           return JsonResponse({'email_exists':"Email Exists. Please try again with new email address."})
-           redirect('employer_registration')
+           message = "Email is already taken."
+           return JsonResponse({"message":"Email is taken"},status=406)
         else:
             try:
-                employer = Employer(emp_name = name,emp_phone_number = number,emp_email = email,emp_password = password)
+                employer = Employer(emp_name = name,emp_phone_number = number,emp_email = normalized_email,emp_password = hashed_password)
                 employer.save()
-                return JsonResponse({'ok':'Account successfully created.'})
+                messages.success(request,"Account Successfully created !")
+                print("Account Created.")
+                return redirect('employer_dashboard')
             except Exception as e:
+                print(e)
                 return JsonResponse ({'error':'An error occured.'})
                 # print("Exception....")
-            
 
     
     return render(request,'forms/employer.html')
@@ -37,3 +42,9 @@ def register_employer(request):
 
 def employer_signin(request):
     return render(request,'forms/employer_login.html')
+
+
+
+
+def employer_dashboard(request):
+    return HttpResponse(request,"<h1>Welcome home</h1>");
