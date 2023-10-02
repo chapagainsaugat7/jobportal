3b0c7f6cd27214589997cd419c6ca99bd317a6a4
Rajuxz <rajankhanalxz@gmail.com>
2023-10-02 22:42:55 +0545

    Fixed: 404 image not found error

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('',include('core.urls')),
     path('jobseeker/',include('jobseeker.urls')),
     path('employer/',include('employer.urls')),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
