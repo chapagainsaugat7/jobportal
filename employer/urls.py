2d7c446388b4c61e9a3f3c737a0e5195778c3fe0
Rajuxz <rajankhanalxz@gmail.com>
2023-10-05 10:06:06 +0545

    Conf: Url configuration for post jobs

--- employer/urls.py
+++ employer/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register_employer/',register_employer,name='employer_registration'),
     path('login_employer/',employer_signin,name="employer_signin"),
     path('employer_dashboard/',employer_dashboard,name="employer_dashboard"),
-    path('company_profile/',company_profile,name="company_profile")
+    path('company_profile/',company_profile,name="company_profile"),
+    path("postjobs/", post_jobs, name="postjobs")
 ]
