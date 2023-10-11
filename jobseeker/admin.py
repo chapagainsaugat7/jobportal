2a7cfb74128fe106c7c0a4d8e9302401162eef4c
Rajuxz <rajankhanalxz@gmail.com>
2023-10-11 19:42:25 +0545

    Registering Appliedjobs model

--- jobseeker/admin.py
+++ jobseeker/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from jobseeker.models import JobSeeker
+from jobseeker.models import JobSeeker,AppliedJobs
 # Register your models here.
 
 admin.site.register(JobSeeker)
+admin.site.register(AppliedJobs)
