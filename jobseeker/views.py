07821ba9b6503022167337b8713d5a2fc2cffbb4
Rajuxz <rajankhanalxz@gmail.com>
2023-10-12 15:29:43 +0545

    Views

--- jobseeker/views.py
+++ jobseeker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.hashers import make_password,check_password
 from django.contrib import messages
 from jobseeker.models import JobSeeker
+from employer.models import Job
 import uuid
 
 def register_job_seeker(request):
@@ -12,10 +13,11 @@ def register_job_seeker(request):
         password = request.POST.get('jpassword')
         normalized_email = email.lower()
         hashed_password  = make_password(password)
-        message = ''
+
         email_exists = JobSeeker.objects.filter(email = normalized_email).exists()
         phone_exists = JobSeeker.objects.filter(phone = phone_number).exists()
 
+        message = ''
         if email_exists:
             message = 'Email Already Exists'
         elif phone_exists:
@@ -61,12 +63,28 @@ def login(request):
             return redirect('login')
 
         else:
-            jobseeker = JobSeeker.object.get(email = email)
-
+            jobseeker_exists = JobSeeker.objects.filter(email = normalized_email).exists()
+            if jobseeker_exists:
+                jobseeker = JobSeeker.objects.get(email = normalized_email)
+                password_matched = check_password(password,jobseeker.password)
+                if password_matched:
+                    return redirect('jobseeker-dashboard')
+                else:
+                    messages.error(request,"Password doesn't matched.")
+                    return redirect('login')
+            else:
+                messages.error(request,"Account with provided email doesn't exists.")
+                return redirect('register_jobseeker')
     return render(request,'forms/login.html')
 
+
 def jobseeker_dashboard(request):
-    return render(request,'jobseeker-dashboard/dashboard.html')
+    email = request.session['email']
+    if email:
+        return render(request,'jobseeker-dashboard/dashboard.html')
+    else:
+        messages.error(request,"Session Expired. Please login here.")
+        return redirect('login')
 
 def create_profile(request):
     nav_items = ["Home","Qualification","Preferences"]
