6e5a70a95abb56cf7462556c32c0a3f5fa8790e6
Rajuxz <rajankhanalxz@gmail.com>
2023-10-12 23:29:09 +0545

    Fixed: Conflicting urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',home,name="home_page"),
     path('admin-login',admin_login,name='admin'),
     path('jobportal-admin',dashboard,name='jobportal-dashboard'),
-    path('admin_home/',admin_home,name="home_page"),
+    path('admin_home/',admin_home,name="admindashboard"),
     path('statistics',statistics,name="statistics"),
 ]
