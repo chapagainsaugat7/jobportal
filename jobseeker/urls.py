a7c9aa37e7fec54cca5b0cf9c9c69fd57b2bb336
Rajuxz <rajankhanalxz@gmail.com>
2023-10-19 19:38:52 +0545

    Url for getting quizes configured

--- jobseeker/urls.py
+++ jobseeker/urls.py
@@ -4,12 +4,13 @@ urlpatterns = [
     path('',jobseeker_dashboard,name="jobseeker-dashboard"),
     path('register-job-seeker/',register_job_seeker,name='register_jobseeker'),
     path('login/',login,name="login"),
-    path('create-profile',create_profile,name="create-profile"),
+    path('statistics',statistics,name="statistics"),
     path('profile',profile,name="profile"),
     path('logout',logout_jobseeker,name='logout'),
     path('browsejob/<int:id>',browse_job,name="browsejob"),
     path('view_employer/<int:employer>',view_employer,name="view_employer"),
     path('downloadcv/<uuid:jobseeker_id>',download_cv,name="downloadcv"),
     path('applyjob/',applyjob,name='applyjob'),
-    path('quiz/<int:id>',quiz,name='quiz')
+    path('quiz/<int:id>',quiz,name='quiz'),
+    path('getquizes/<int:job_id>/<int:employer>',get_quizes,name='getquizes')
 ]
