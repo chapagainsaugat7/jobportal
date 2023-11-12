99085fc8cbee388db37ae4d7f409d22a62a06f0b
Rajuxz <rajankhanalxz@gmail.com>
2023-11-12 12:16:56 +0545

    Adding admin control fields

--- jobseeker/admin.py
+++ jobseeker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from jobseeker.models import JobSeeker,AppliedJobs,Score
+from jobseeker.models import JobSeeker,AppliedJobs,Score,ShortListedCandidates
 # Register your models here.
 
 admin.site.register(JobSeeker)
 admin.site.register(AppliedJobs)
 admin.site.register(Score)
+admin.site.register(ShortListedCandidates)
