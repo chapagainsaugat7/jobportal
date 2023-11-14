b9b7cf380bff5cc689e398043e1f5ec8f7a2f855
Rajuxz <rajankhanalxz@gmail.com>
2023-11-14 09:03:13 +0545

    Setting hosts and debug to false.

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8y*(ykjs28u+51&jeg9u$@(0*t^9^d6+s+y$d9i+b^_u!+=58#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
