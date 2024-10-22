d575045800f6e67374fb66e7956b5bad17ebe01e
Saugat Chapagain <saugatchapagain7@gmail.com>
2024-10-22 13:34:42 +0545

    final

--- jobseeker/views.py
+++ jobseeker/views.py
@@ -14,6 +14,7 @@ from io import BytesIO
 from django.template.loader import get_template
 from django.db.models import Q
 from django.urls import reverse
+from .models import Job
 import uuid
 import json
 import datetime,time
